f094a33b4154c2cc5eecfe38df45deda9206cd3f
振强 李 <lizhenqiang@ipubtrans.cn>
2017-08-03 14:37:54 +0800

    添加显示版本号 v1.2.0

--- app/src/main/java/com/cangjie/mayday/ui/MyFragment.java
+++ app/src/main/java/com/cangjie/mayday/ui/MyFragment.java
@@ -1,15 +1,19 @@
 package com.cangjie.mayday.ui;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.cangjie.basetool.mvp.base.BaseHeadFragment;
 import com.cangjie.basetool.utils.ToastHelper;
 import com.cangjie.mayday.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -21,6 +25,8 @@ public class MyFragment extends BaseHeadFragment {
     private View rootView;
     Context mContext;
 
+    @Bind(R.id.tv_version)
+    TextView tv_version;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,8 +44,22 @@ public class MyFragment extends BaseHeadFragment {
         rootView = setContentView(rootView);
         ButterKnife.bind(this, rootView);
         showTitle("更多");
+
+        try {
+            initVersion();
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
         return rootView;
     }
+
+    private void initVersion() throws PackageManager.NameNotFoundException {
+        PackageInfo packageInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
+        String versionName = packageInfo.versionName;
+        tv_version.setText(getString(R.string.version, versionName));
+
+    }
+
     @OnClick({R.id.ll_user_info, R.id.ll_credits_exchange,R.id.ll_activity,R.id.ll_auto,R.id.ll_version,R.id.ll_suggest})
     public void onItemClick(View view){
         ToastHelper.showToast("正在开发中", mContext);
